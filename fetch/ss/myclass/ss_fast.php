512cc480d6c965e2bea65a3dd6de4fc7a309b8c6
zhaohongyu <zhao_hong_yu@sina.cn>
2016-10-10 22:01:16 +0800

    33

--- fetch/ss/myclass/ss_fast.php
+++ fetch/ss/myclass/ss_fast.php
@@ -12,6 +12,12 @@ require_once('./myclass/SsInfo.php');
 
 class ss_fast extends Base {
 
+    public function __construct($fetch_url = '', $user_cookie = '') {
+        $fetch_url   = empty($fetch_url) ? "https://www.ss-fast.com/ucenter/?act=free_plan" : $fetch_url;
+        $user_cookie = empty($user_cookie) ? "ss_secret=6bbe%2B%2BSA8RBmuwBNmtdQe1OiuX3Cf7iOOSR3BDgCGzVnhmvvU9%2FDIUhR22rNNUWGbVmIzR57%2BH7O3jJnIjFH6miWR3PB1e2nny7179HQg9%2FRRlHKZE16HRRrexoTdH6oKHb6x3MGaS1gdF%2B8dbqS; PHPSESSID=fd0b2794949b7076a8b0103cbde59454" : $user_cookie;
+        parent::__construct($fetch_url, $user_cookie);
+    }
+
     public function getHtml() {
 
         // Get cURL resource
@@ -61,7 +67,7 @@ class ss_fast extends Base {
         if (empty($matches3[1])) {
             return null;
         }
-        
+
         $ss_info_arr  = $matches3[1];
         $domain       = $ss_info_arr[2];
         $encrypt_type = $ss_info_arr[4];
