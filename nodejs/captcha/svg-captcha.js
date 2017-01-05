e6bbfc40d02f1713f753b206aa05cd315871d5d7
zhaohongyu <zhao_hong_yu@sina.cn>
2017-01-05 20:11:28 +0800

    no message

--- nodejs/captcha/svg-captcha.js
+++ nodejs/captcha/svg-captcha.js
@@ -4,6 +4,7 @@
 
 var http       = require('http');
 var svgCaptcha = require('svg-captcha');
+var fs         = require('fs');
 
 http.createServer(function (request, response) {
     if (request.url == '/captcha') {
@@ -11,11 +12,18 @@ http.createServer(function (request, response) {
         var captcha = svgCaptcha.create();
         console.log(captcha);
         response.writeHead(200, {
-            'Content-Type': 'image/svg+xml'
+            'Content-Type': 'image/svg+xml',
+            'token'       : Math.random()
         });
         response.end(captcha.data);
 
-    } else response.end('');
+    }
+
+    if (request.url == '/sav-captcha.html') {
+        var html = fs.readFileSync("./sav-captcha.html", "utf-8");
+        response.end(html);
+    }
+
 }).listen(8181);
 
 console.log('Web server started.\n http://127.0.0.1:8181/captcha');
