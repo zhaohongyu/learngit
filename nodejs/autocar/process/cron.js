af88c2eca592306e1c283a47167b6223b06033a2
zhaohongyu <zhao_hong_yu@sina.cn>
2017-05-25 20:59:13 +0800

    no message

--- nodejs/autocar/process/cron.js
+++ nodejs/autocar/process/cron.js
@@ -13,45 +13,52 @@ const delay            = 15;// 秒
 function Cron() {}
 
 Cron.prototype.myApply = Promise.coroutine(function*(val) {
-    yield Promise.delay(delay * 1000);
+    try {
+        yield Promise.delay(delay * 1000);
 
-    const entercarlist = yield SendRequest.entercarlist();
+        const entercarlist = yield SendRequest.entercarlist();
 
-    if (_.isEmpty(entercarlist)) {
-        return this.myApply(DateUtil.currentDateTime());
-    }
-
-    if (entercarlist.rescode !== "200") {
-        console.log('获取车辆列表失败了,错误信息是:', JSON.stringify(entercarlist));
-        return this.myApply(DateUtil.currentDateTime());
-    }
-
-    const datalist = entercarlist.datalist;
-    for (let myCarInfo of datalist) {
-
-        let carapplyData   = myCarInfo.carapplyarr[0];
-        let newCarapplyarr = new CarapplyarrClass(carapplyData);
-
-        let newMyCarInfoData         = _.pick(myCarInfo, ['carid', 'userid', 'licenseno ', 'applyflag']);
-        newMyCarInfoData.carapplyarr = [newCarapplyarr];
-        let newCar                   = new CarInfoClass(newMyCarInfoData);
-
-        if (newCarapplyarr.isPass()) {
-            console.log(newCarapplyarr.licenseno + '申请通过...');
-            continue;
+        if (_.isEmpty(entercarlist)) {
+            return this.myApply(DateUtil.currentDateTime());
         }
 
-        if (!newCar.canRequest()) {
-            console.log(newCarapplyarr.licenseno + '当前状态不能不能申请操作...');
-            continue;
+        if (entercarlist.rescode !== "200") {
+            console.log('获取车辆列表失败了,错误信息是:', JSON.stringify(entercarlist));
+            return this.myApply(DateUtil.currentDateTime());
         }
 
-        // todo 申请操作
-        const addcartype = yield SendRequest.addcartype(newCar);
+        const datalist = entercarlist.datalist;
+        for (let myCarInfo of datalist) {
+
+            let carapplyData   = myCarInfo.carapplyarr[0];
+            let newCarapplyarr = new CarapplyarrClass(carapplyData);
+
+            let newMyCarInfoData         = _.pick(myCarInfo, ['carid', 'userid', 'licenseno ', 'applyflag']);
+            newMyCarInfoData.carapplyarr = [newCarapplyarr];
+            let newCar                   = new CarInfoClass(newMyCarInfoData);
+
+            if (newCarapplyarr.isPass()) {
+                console.log(newCarapplyarr.licenseno + '申请通过...');
+                continue;
+            }
+
+            if (!newCar.canRequest()) {
+                console.log(newCarapplyarr.licenseno + '当前状态不能不能申请操作...');
+                continue;
+            }
+
+            // todo 申请操作
+            const addcartype = yield SendRequest.addcartype(newCar);
+
+        }
+
+        return this.myApply(DateUtil.currentDateTime());
 
     }
-
-    return this.myApply(DateUtil.currentDateTime());
+    catch (error) {
+        console.log('myApply 出错了,报错信息是:', error.message);
+        return this.myApply(DateUtil.currentDateTime());
+    }
 });
 
 const cron = new Cron();
