1a76535b0b86cc5bb16e49b4ae71964b8069cd5e
zhaohongyu <zhao_hong_yu@sina.cn>
2017-08-30 15:24:29 +0800

    生成数据字典

--- script/data_dictionary.php
+++ script/data_dictionary.php
@@ -1,99 +1,148 @@
 <?php
 
-/**
- * 生成mysql数据字典
- */
+// 生成mysql数据字典
 header("Content-type: text/html; charset=utf-8");
 
 // 配置数据库
-$dbserver = "localhost";
-$dbusername = "root";
-$dbpassword = "";
-$database = isset($_GET['database']) ? $_GET['database'] : "andsonweb";
+$host     = "192.168.100.60";
+$username = "root";
+$password = "";
+$database = isset($_GET['database']) ? $_GET['database'] : "event_dove_admin";
 
-// 其他配置
-$title = "$database-数据字典";
+$tables = [];
+$title  = "$database-数据字典";
 
-$mysql_conn = @mysql_connect("$dbserver", "$dbusername", "$dbpassword") or die("Mysql connect is error.");
-if (!mysql_select_db($database, $mysql_conn)) {
-    die($database . '数据库不存在!');
-}
-mysql_query('SET NAMES utf8', $mysql_conn);
-$table_result = mysql_query('show tables', $mysql_conn);
-// 取得所有的表名
-while ($row = mysql_fetch_array($table_result)) {
-    $tables [] ['TABLE_NAME'] = $row [0];
+// PDO
+$pdo       = new PDO("mysql:host={$host};dbname={$database}", $username, $password);
+$statement = $pdo->query("show tables");
+
+// 组装表名数组
+while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+    $tables [] ['TABLE_NAME'] = $row["Tables_in_{$database}"];
 }
 
 // 循环取得所有表的备注及表中列消息
 foreach ($tables as $k => $v) {
-    $sql = 'SELECT * FROM ';
-    $sql .= 'INFORMATION_SCHEMA.TABLES ';
-    $sql .= 'WHERE ';
-    $sql .= "table_name = '{$v['TABLE_NAME']}'  AND table_schema = '{$database}'";
-    $table_result = mysql_query($sql, $mysql_conn);
-    while ($t = mysql_fetch_array($table_result)) {
+    $sql1         = 'SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE ';
+    $sql1         .= "table_name = '{$v['TABLE_NAME']}'  AND table_schema = '{$database}'";
+    $table_result = $pdo->query($sql1);
+    while ($t = $table_result->fetch(PDO::FETCH_ASSOC)) {
         $tables [$k] ['TABLE_COMMENT'] = $t ['TABLE_COMMENT'];
     }
 
-    $sql = 'SELECT * FROM ';
-    $sql .= 'INFORMATION_SCHEMA.COLUMNS ';
-    $sql .= 'WHERE ';
-    $sql .= "table_name = '{$v['TABLE_NAME']}' AND table_schema = '{$database}'";
-
-    $fields = array();
-    $field_result = mysql_query($sql, $mysql_conn);
-    while ($t = mysql_fetch_array($field_result)) {
+    $sql2         = 'SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE ';
+    $sql2         .= "table_name = '{$v['TABLE_NAME']}' AND table_schema = '{$database}'";
+    $fields       = array();
+    $field_result = $pdo->query($sql2);
+    while ($t = $field_result->fetch(PDO::FETCH_ASSOC)) {
         $fields[] = $t;
     }
     $tables [$k] ['COLUMN'] = $fields;
 }
-mysql_close($mysql_conn);
 
-$html = '';
-// 循环所有表
+$dictionary_html_1
+    = <<<html_1
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>{$title}</title>
+        <link rel="stylesheet" href="http://apps.bdimg.com/libs/bootstrap/3.3.4/css/bootstrap.min.css">
+        <script src="http://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js"></script>
+        <script src="http://apps.bdimg.com/libs/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+        <style type="text/css">
+            ul.nav-tabs{
+                width: 280px;
+                margin-top: 20px;
+                border-radius: 4px;
+                border: 1px solid #ddd;
+                box-shadow: 0 1px 4px rgba(0, 0, 0, 0.067);
+            }
+            ul.nav-tabs li{
+                margin: 0;
+                border-top: 1px solid #ddd;
+            }
+            ul.nav-tabs li:first-child{
+                border-top: none;
+            }
+            ul.nav-tabs li a{
+                margin: 0;
+                padding: 8px 16px;
+                border-radius: 0;
+            }
+            ul.nav-tabs li.active a, ul.nav-tabs li.active a:hover{
+                color: #fff;
+                background: #0088cc;
+                border: 1px solid #0088cc;
+            }
+            ul.nav-tabs li:first-child a{
+                border-radius: 4px 4px 0 0;
+            }
+            ul.nav-tabs li:last-child a{
+                border-radius: 0 0 4px 4px;
+            }
+            ul.nav-tabs.affix{
+                top: 30px; /* Set the top position of pinned element */
+            }
+        </style>
+    </head>
+    <body data-spy="scroll" data-target="#myScrollspy">
+    <div class="container">
+    <div class="row">
+        <div class="col-xs-3" id="myScrollspy">
+            <ul class="nav nav-tabs nav-stacked affix" data-spy="affix" data-offset-top="125">
+html_1;
+
 foreach ($tables as $k => $v) {
-// $html .= '<p><h2>'. $v['TABLE_COMMENT'] . '&nbsp;</h2>';
-    $html .= '<table  border="1" cellspacing="0" cellpadding="0" align="center">';
-    $html .= '<caption>' . $v ['TABLE_NAME'] . '  ' . $v ['TABLE_COMMENT'] . '</caption>';
-    $html .= '<tbody><tr><th>字段名</th><th>数据类型</th><th>默认值</th>
-    <th>允许非空</th>
-    <th>自动递增</th><th>备注</th></tr>';
-    $html .= '';
-
-    foreach ($v ['COLUMN'] as $f) {
-        $html .= '<tr><td class="c1">' . $f ['COLUMN_NAME'] . '</td>';
-        $html .= '<td class="c2">' . $f ['COLUMN_TYPE'] . '</td>';
-        $html .= '<td class="c3">&nbsp;' . $f ['COLUMN_DEFAULT'] . '</td>';
-        $html .= '<td class="c4">&nbsp;' . $f ['IS_NULLABLE'] . '</td>';
-        $html .= '<td class="c5">' . ($f ['EXTRA'] == 'auto_increment' ? '是' : '&nbsp;' ) . '</td>';
-        $html .= '<td class="c6">&nbsp;' . $f ['COLUMN_COMMENT'] . '</td>';
-        $html .= '</tr>';
-    }
-    $html .= '</tbody></table></p>';
+    $section_title     = $v['TABLE_NAME'];
+    $dictionary_html_1 .= "<li><a href='#{$section_title}'>{$section_title}</a></li>";
 }
 
-// 输出
-echo '<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>' . $title . '</title>
-<style>
-body,td,th {font-family:"宋体"; font-size:12px;}
-table{border-collapse:collapse;border:1px solid #CCC;background:#6089D4;}
-table caption{text-align:left; background-color:#fff; line-height:2em; font-size:14px; font-weight:bold; }
-table th{text-align:left; font-weight:bold;height:26px; line-height:25px; font-size:16px; border:3px solid #fff; color:#ffffff; padding:5px;}
-table td{height:25px; font-size:12px; border:3px solid #fff; background-color:#f0f0f0; padding:5px;}
-.c1{ width: 150px;}
-.c2{ width: 130px;}
-.c3{ width: 70px;}
-.c4{ width: 80px;}
-.c5{ width: 80px;}
-.c6{ width: 300px;}
-</style>
-</head>
-<body>';
-echo '<h1 style="text-align:center;">' . $title . '</h1>';
-echo $html;
-echo '</body></html>';
-?>
+$dictionary_html_2
+    = <<<html_2
+            </ul>
+       </div>
+       <div class='col-xs-9'>
+html_2;
+
+foreach ($tables as $k => $v) {
+    $section_title = $v['TABLE_NAME'];
+    $table_comment = $v['TABLE_COMMENT'];
+
+    $html = "";
+    $html .= "<table class='table table-responsive table-bordered table-hover table-condensed table-striped'>";
+    $html .= "<caption>{$table_comment}</caption>";
+    $html .= "<tbody><tr><th>字段名</th><th>数据类型</th><th>默认值</th> <th>允许非空</th> <th>自动递增</th><th>备注</th></tr>";
+
+    foreach ($v ['COLUMN'] as $f) {
+        $is_auto_increment = ($f ['EXTRA'] === 'auto_increment') ? '是' : '';
+        $html              .= "<tr>";
+        $html              .= "    <td>{$f ['COLUMN_NAME']}</td>";
+        $html              .= "    <td>{$f ['COLUMN_TYPE']}</td>";
+        $html              .= "    <td>{$f ['COLUMN_DEFAULT']}</td>";
+        $html              .= "    <td>{$f ['IS_NULLABLE']}</td>";
+        $html              .= "    <td>{$is_auto_increment}</td>";
+        $html              .= "    <td>{$f ['COLUMN_COMMENT']}</td>";
+        $html              .= '</tr>';
+    }
+    $html .= '</tbody></table>';
+
+    $dictionary_html_2
+        .= <<<html_2
+            <h2 id="{$section_title}">{$section_title}</h2>
+            {$html}
+            <hr>
+html_2;
+
+}
+
+$dictionary_html_3
+    = <<<html_3
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+html_3;
+
+echo $dictionary_html_1 . $dictionary_html_2 . $dictionary_html_3;
